9d33430548f1a3c6c6e0865577836117549b28bc
Gyan Prakash Rai <shanuraig@gmail.com>
2025-06-06 15:12:28 +0530

    Update palindrome_LL.cpp

--- LinkedList/Medium/palindrome_LL.cpp
+++ LinkedList/Medium/palindrome_LL.cpp
@@ -10,6 +10,8 @@ node(int val){
 }
 };
 //better solution
+class solution{
+public: 
 bool ispalindrome(node* head){
     stack<int> st;
     node*temp=head;
@@ -25,4 +27,37 @@ bool ispalindrome(node* head){
     }
     return true;
 }
-//optimal solution 
+//optimal solution two pointer approach
+bool palindrome(node* head){
+    node* slow=head;
+    node* fast=head;
+    while(fast!=nullptr && fast->next!=nullptr){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    if(fast!=nullptr){
+        slow=slow->next;
+        slow=reverselist(slow);
+    }
+    while(slow!=nullptr){
+        if(slow->data != head->data ) return false;
+        slow=slow->next;
+        head=head->next;
+    }
+    return true;
+}
+private:
+node* reverselist(node* head){
+    node* prev=nullptr;
+    node*curr=head;
+    while(curr!=nullptr){
+        node*next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    return prev;
+}
+};
+
+
