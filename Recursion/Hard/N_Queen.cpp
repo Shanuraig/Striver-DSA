7a24b0400c22c6f4ab67196893b56c850b228667
Gyan Prakash Rai <shanuraig@gmail.com>
2025-07-24 02:37:14 +0530

    Update N_Queen.cpp

--- Recursion/Hard/N_Queen.cpp
+++ Recursion/Hard/N_Queen.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 class Solution {
 public:
+bool issafe(int row , int col , vector<vector<int>> &board ,int n){
+
+}
     vector<vector<string>> solveNQueens(int n) {
         
     }
