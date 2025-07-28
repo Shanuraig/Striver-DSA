6bf2b81d9ab97cea7a39463882f1350b996b07c6
Gyan Prakash Rai <shanuraig@gmail.com>
2025-07-28 22:58:39 +0530

    Update N_Queen.cpp

--- Recursion/Hard/N_Queen.cpp
+++ Recursion/Hard/N_Queen.cpp
@@ -2,10 +2,55 @@
 using namespace std;
 class Solution {
 public:
-bool issafe(int row , int col , vector<vector<int>> &board ,int n){
-
+bool canfill(int row,int col,vector<string> &curr,int n){
+    int drow=row;
+    int dcol=col;
+    while(row>=0 && col>=0){
+        if(curr[row][col]=='Q'){
+            return false;
+        }
+        row--;
+        col--;
+    }
+    col=dcol;
+    row=drow;
+    while(col>=0){
+         if(curr[row][col]=='Q'){
+            return false;
+        }
+        col--;
+    }
+    col=dcol;
+    while(row<n && col>=0){
+        if(curr[row][col]=='Q'){
+            return false;
+        }
+        row++;
+        col--;
+    }
+    return true;
+}
+void solve(int col , vector<string> &curr , vector<vector<string>> & result , int n){
+        if(col==n){
+            result.push_back(curr);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(canfill(row,col,curr,n)){
+                curr[row][col]='Q';
+                solve(col+1,curr,result,n);
+                curr[row][col]='.';
+            }
+        }
 }
     vector<vector<string>> solveNQueens(int n) {
-        
+        vector<vector<string>> result;
+        vector<string> curr(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            curr[i]=s;
+        }
+        solve(0,curr,result,n);
+        return result;
     }
 };
